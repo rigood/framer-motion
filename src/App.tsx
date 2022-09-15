7defaa59ffa78b8de6316e52a4b0732bd592197b
rigood <rigood@naver.com>
2022-09-15 23:45:11 +0900

    LayoutId overlay example

--- src/App.tsx
+++ src/App.tsx
@@ -14,29 +14,52 @@ const Box = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 400px;
-  height: 400px;
+  height: 300px;
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   background-color: rgba(255, 255, 255, 1);
   font-size: 40px;
 `;
 
-const Circle = styled(motion.div)`
-  width: 100px;
-  height: 100px;
-  border-radius: 50px;
-  background-color: #00a5ff;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  width: 50vw;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
 `;
 
 export default function App() {
   const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked((prev) => !prev);
+  const toggle = () => setClicked((prev) => !prev);
   return (
-    <Wrapper onClick={toggleClicked}>
-      <Box>{clicked ? null : <Circle layoutId="circle" style={{ borderRadius: 50 }} />}</Box>
-      <Box>{!clicked ? null : <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />}</Box>
+    <Wrapper onClick={toggle}>
+      <Grid>
+        <Box layoutId="merge" />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
+        {" "}
+        {clicked ? (
+          <Overlay initial={{ backgroundColor: "rgba(0,0,0,0)" }} animate={{ backgroundColor: "rgba(0,0,0,0.5)" }} exit={{ backgroundColor: "rgba(0,0,0,0)" }}>
+            <Box layoutId="merge" style={{ width: 400, height: 200 }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
