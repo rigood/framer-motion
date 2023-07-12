09e2be7685cb427612b12b3bcefe4902f545e2c3
rigood <rigood@naver.com>
2023-07-13 01:01:43 +0900

    ♻️ Refator code and install deploy package

--- README.md
+++ README.md
@@ -1,46 +1,19 @@
-# Getting Started with Create React App
+# Framer Motion 애니메이션 연습
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## 1. Box와 Modal을 자연스럽게 연결하기
 
-## Available Scripts
+- `layoutId`를 이용하여 Box와 Modal을 자연스럽게 연결할 수 있습니다.
+- `whileHover`를 통해 hover 시 스타일을 지정할 수 있습니다.
 
-In the project directory, you can run:
+<img src="public/assets/readme/box.gif" width="300"/>
 
-### `npm start`
+<br/>
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+## 2. 공 옮기기
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+- `layoutId`를 이용하여 Circle을 자연스럽게 switch 할 수 있습니다.
 
-### `npm test`
+<img src="public/assets/readme/circle.gif" width="400"/>
+<img src="public/assets/readme/responsive.gif" width="200"/>
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+</br>
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,122 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
-import { useState } from "react";
+import { motion } from "framer-motion";
+import { useState, useRef } from "react";
+
+export default function App() {
+  const [id, setId] = useState<null | string>();
+
+  const [switched, setSwitched] = useState(false);
+  const toggleBtn = () => setSwitched((prev) => !prev);
+
+  const boxRef = useRef<HTMLDivElement>(null);
+  const getBoxWidth = () => {
+    if (!boxRef) return;
+
+    return boxRef.current?.offsetWidth;
+  };
+
+  return (
+    <Wrapper>
+      <Grid>
+        <Box
+          onClick={() => setId("first")}
+          layoutId={"first"}
+          whileHover={{ scale: 1.1 }}
+          ref={boxRef}
+        >
+          Hover me! <br /> Click me!
+        </Box>
+        <Box>{switched ? null : <Circle layoutId="switch" />}</Box>
+        <Box>{switched ? <Circle layoutId="switch" /> : null}</Box>
+        <Box
+          onClick={() => setId("last")}
+          layoutId={"last"}
+          whileHover={{ scale: 1.1 }}
+        >
+          Hover me! <br /> Click me!
+        </Box>
+      </Grid>
+
+      {id ? (
+        <Overlay
+          onClick={() => setId(null)}
+          initial={{ backgroundColor: "rgba(0,0,0,0)" }}
+          animate={{ backgroundColor: "rgba(0,0,0,0.5)" }}
+          exit={{ backgroundColor: "rgba(0,0,0,0)" }}
+        >
+          <Modal layoutId={id} width={getBoxWidth()!}>
+            Thank you 🥰
+          </Modal>
+        </Overlay>
+      ) : null}
+
+      <Button
+        whileHover={{
+          scale: 1.1,
+          transition: { duration: 0.3 },
+        }}
+        onClick={toggleBtn}
+      >
+        공 옮기기
+      </Button>
+    </Wrapper>
+  );
+}
 
 const Wrapper = styled.div`
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 100vw;
-  height: 100vh;
 `;
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(2, minmax(100px, 400px));
   gap: 20px;
-  margin-bottom: 50px;
+  margin: 20px 10% 60px;
 `;
 
 const Box = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 400px;
+  text-align: center;
   height: 250px;
   border-radius: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
   background-color: rgba(255, 255, 255, 0.5);
+  font-weight: bold;
+
+  &:first-child,
+  &:last-child {
+    cursor: pointer;
+    background-color: rgba(216, 159, 202, 0.5);
+  }
+
+  &:first-child {
+    transform-origin: bottom right !important;
+    // whileHover에 originX: "400px", originY: "250px" -> 클릭하고 난 뒤에 origin값이 초기화됨
+    // !important 키워드를 사용하지 않으면 Modal 띄운 후 transform-origin이 기본값으로 초기화됨
+  }
+
+  &:last-child {
+    transform-origin: top left !important;
+    // whileHover에 originX: 0, originY: 0 지정하면 클릭하고 난 뒤에 origin값이 초기화됨
+    // !important 키워드를 사용하지 않으면 Modal 띄운 후 transform-origin이 기본값으로 초기화됨
+  }
+`;
+
+const Modal = styled(motion.div)<{ width: number }>`
+  width: ${({ width }) => width + "px"};
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  background-color: white;
+  font-weight: bold;
   cursor: pointer;
 `;
 
@@ -38,53 +128,21 @@ const Circle = styled(motion.div)`
 `;
 
 const Button = styled(motion.button)`
-  padding: 10px;
+  padding: 10px 20px;
   border: none;
   border-radius: 5px;
-  font-size: 18px;
   font-weight: bold;
-  color: #228be6;
+  font-size: 20px;
   cursor: pointer;
 `;
 
 const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-  position: absolute;
-  width: 100%;
-  height: 100%;
 `;
-
-export default function App() {
-  const [switched, setSwitched] = useState(false);
-  const [id, setId] = useState<null | string>();
-  const toggleBtn = () => setSwitched((prev) => !prev);
-  return (
-    <Wrapper>
-      <Grid>
-        <Box onClick={() => setId("1")} layoutId={"1"} whileHover={{ scale: 1.1, originX: "400px", originY: "250px" }} />
-        <Box>{switched ? null : <Circle layoutId="switch" />}</Box>
-        <Box>{switched ? <Circle layoutId="switch" /> : null}</Box>
-        <Box onClick={() => setId("4")} layoutId={"4"} whileHover={{ scale: 1.1, originX: "0", originY: "0" }} />
-      </Grid>
-      <AnimatePresence>
-        {id ? (
-          <Overlay onClick={() => setId(null)} initial={{ backgroundColor: "rgba(0,0,0,0)" }} animate={{ backgroundColor: "rgba(0,0,0,0.5)" }} exit={{ backgroundColor: "rgba(0,0,0,0)" }}>
-            <Box layoutId={id} style={{ backgroundColor: "rgb(255,255,255)" }} />
-          </Overlay>
-        ) : null}
-      </AnimatePresence>
-      <Button
-        whileHover={{
-          scale: 1.2,
-          color: "rgb(245, 159, 0)",
-          transition: { duration: 0.5 },
-        }}
-        onClick={toggleBtn}
-      >
-        Switch
-      </Button>
-    </Wrapper>
-  );
-}
